828d93ada70c3726cc8a753e4052bc0d2bceb5bf
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-12-13 21:39:01 +0200

    Update init code for PostgreSQL.
    Signed-off-by: Artyom Gevorgyan <artemhevorhian@gmail.com>

--- test/main.go
+++ test/main.go
@@ -18,12 +18,15 @@ const (
 	// This user can create databases to bootstrap the db,
 	// password is for this user, AFAIK
 	user = "postgres"
+
+	// This shall be created on opening connection
+	dbname = "test"
 )
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s sslmode=disable",
-		host, port, user, password)
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
@@ -35,17 +38,31 @@ func main() {
 		panic(err)
 	}
 
-	_, err = db.Exec("create database test")
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	_, err = db.Exec("CREATE TABLE distributors (did integer, name varchar(40), PRIMARY KEY(did));")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	_, err = db.Exec("insert into distributors values (1, 'foo');")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	rows, err := db.Query("select distinct table_name from information_schema.columns order by table_name;")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var j string
+		err := rows.Scan(&j)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(j)
+	}
+	err = rows.Err()
 	if err != nil {
 		fmt.Println(err)
 	}
